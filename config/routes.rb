b9d02df287b6f7111522736a749118daad8bfa54
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-09-15 13:42:42 +0000

    [Update]ルーティンの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'show' => 'books#show'
+
+  # post 'books' => 'books#create'
+  # get 'books/index' => 'books#index'
+  # get 'books/'
+
+
   resources :books
   resources :homes
 
-  root to: 'homes#top'
+  # root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
