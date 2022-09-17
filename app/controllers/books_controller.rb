0b3e4a182946868baec23a6919b69ba03ffc0cea
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-09-17 08:26:48 +0000

    [Remove]]ルーティングの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,23 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to books_path(book.id)
+    # ↑後でshowのパスに変更する
   end
 
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'show' => 'books#show'
-
   # post 'books' => 'books#create'
   # get 'books/index' => 'books#index'
   # get 'books/'
 
-
+  get 'top' => 'homes#top'
   resources :books
-  resources :homes
 
   # root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
